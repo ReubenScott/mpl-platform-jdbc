fb8b2a1ce196bbbd7de17f33e28e47568c895930
reubenscott <ReubenLScott@hotmail.com>
2017-10-10 20:39:45 +0800

    update config

--- src/main/java/com/kindustry/framework/dao/imp/BasicDaoImp.java
+++ src/main/java/com/kindustry/framework/dao/imp/BasicDaoImp.java
@@ -14,90 +14,90 @@ import com.kindustry.framework.jdbc.core.JdbcTemplate;
 import com.kindustry.framework.xml.XmlSqlMapper;
 
 /**
- * 
+ * BasicDaoImp
  */
-public class JdbcDaoImp implements IBasicDao {
+public class BasicDaoImp implements IBasicDao {
 
   protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-  protected JdbcTemplate jdbc = JdbcTemplate.getInstance();
+  protected JdbcTemplate jdbcTemplate = JdbcTemplate.getInstance();
 
   public JdbcTemplate getJdbcTemplate() {
-    return jdbc;
+    return jdbcTemplate;
   }
 
-  public void setJdbcHandler(JdbcTemplate jdbcHandler) {
-    this.jdbc = jdbcHandler;
+  public void setJdbcTemplate(JdbcTemplate jdbcHandler) {
+    this.jdbcTemplate = jdbcHandler;
   }
 
   public int execute(String sql, Object... params) {
-    return jdbc.executeUpdate(sql, params);
+    return jdbcTemplate.executeUpdate(sql, params);
   }
 
   public <T> List<T> querySampleList(Class<T> sample, String sql, Object... params) {
-    return jdbc.querySampleList(sample, sql, params);
+    return jdbcTemplate.querySampleList(sample, sql, params);
   }
 
   public List<List> queryForList(String sql, Object... params) {
-    return jdbc.queryForList(sql, params);
+    return jdbcTemplate.queryForList(sql, params);
   }
 
   /**
    * 
    */
   public <T> List<T> findByAnnotatedSample(T annotatedSample, Restrictions... restrictions) {
-    return jdbc.findByAnnotatedSample(annotatedSample, restrictions);
+    return jdbcTemplate.findByAnnotatedSample(annotatedSample, restrictions);
   }
 
   public List findByXmlSqlMapper(String dbalias, String sqlName, String value) {
     String sql = XmlSqlMapper.getInstance().getPreparedSQL(sqlName);
     sql = sql.replaceAll("@date", value);
     logger.debug(sql);
-    return jdbc.queryForList(dbalias, sql);
+    return jdbcTemplate.queryForList(dbalias, sql);
   }
 
   public Workbook exportExcel(String sql, Object... params) {
-    return jdbc.exportNamelessWorkbook(sql, params);
+    return jdbcTemplate.exportNamelessWorkbook(sql, params);
   }
 
   public HashMap queryOneAsMap(String sql, Object... params) {
-    return jdbc.queryOneAsMap(sql, params);
+    return jdbcTemplate.queryOneAsMap(sql, params);
   }
 
   public HashMap queryOneAsMap(String dbalias, String sql, Object... params) {
-    return jdbc.queryOneAsMap(dbalias, sql, params);
+    return jdbcTemplate.queryOneAsMap(dbalias, sql, params);
   }
 
   public void exportCSV(String filePath, String encoding, char split, String sql, Object... params) {
-    jdbc.exportCSV(filePath, encoding, split, sql, params);
+    jdbcTemplate.exportCSV(filePath, encoding, split, sql, params);
   }
 
   public boolean saveAnnotatedBean(Object... annoBean) {
-    return jdbc.saveAnnotatedBean(annoBean);
+    return jdbcTemplate.saveAnnotatedBean(annoBean);
   }
 
   public boolean saveAnnotatedBean(List<?> annoBeans) {
-    return jdbc.saveAnnotatedBean(BeanUtil.listToArray(annoBeans));
+    return jdbcTemplate.saveAnnotatedBean(BeanUtil.listToArray(annoBeans));
   }
 
   public boolean truncateTable(String schema, String tablename) {
-    return jdbc.truncateTable(schema, tablename);
+    return jdbcTemplate.truncateTable(schema, tablename);
   }
   
   public  boolean wipeTableData(Class<? extends Object> entityClass){
-    return jdbc.truncateAnnotatedTable(entityClass);
+    return jdbcTemplate.truncateAnnotatedTable(entityClass);
   }
 
   public void loadExcelFile(String tablename, String filePath) {
-    jdbc.loadExcelFile(null, tablename, filePath);
+    jdbcTemplate.loadExcelFile(null, tablename, filePath);
   }
 
   public List callProcedure(String procedureName, Object[] in, int... outTypes) {
-    return jdbc.callProcedure(procedureName, in, outTypes);
+    return jdbcTemplate.callProcedure(procedureName, in, outTypes);
   }
 
   public boolean deleteAnnotatedEntity(Object annoEntity) {
-    return jdbc.deleteAnnotatedBean(annoEntity);
+    return jdbcTemplate.deleteAnnotatedBean(annoEntity);
   }
 
 }
